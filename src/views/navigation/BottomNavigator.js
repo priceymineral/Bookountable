b20a4443116491ea2ab26548723462f6f19a2303
Octavio <tudaeva@gmail.com>
2021-01-19 12:39:53 -0800

    reformatted BottomNavigator

--- src/views/navigation/BottomNavigator.js
+++ src/views/navigation/BottomNavigator.js
@@ -70,7 +70,7 @@ export default function BottomNavigator ({navigation}) {
           ),
         }}
       />
-            <Tab.Screen
+      <Tab.Screen
         name='LocalMall'
         component={HomeScreen}
         options={{
